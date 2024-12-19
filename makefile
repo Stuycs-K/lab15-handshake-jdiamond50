941ed79fb9977fea30ff6a1d1eb9e557f81bf7cd
Julien Diamond <jdiamond50@stuy.edu>
2024-12-19 15:35:43 -0500

    server_setup() and beginning of client_handshake() written but untested

--- makefile
+++ makefile
@@ -1,6 +1,7 @@
 all: client server
+	
 client: basic_client.o pipe_networking.o
-  gcc -o client basic_client.o pipe_networking.o
+	gcc -o client basic_client.o pipe_networking.o
 
 server: basic_server.o pipe_networking.o
 	gcc -o server basic_server.o pipe_networking.o
@@ -15,5 +16,4 @@ pipe_networking.o: pipe_networking.c pipe_networking.h
 	gcc -c pipe_networking.c
 
 clean:
-	rm *.o
-	rm *~
+	rm -f *.o *~ client server
--- pipe_networking.c
+++ pipe_networking.c
@@ -11,11 +11,30 @@
   =========================*/
 int server_setup() {
   int from_client = 0;
+
+  // creatintg WKP
+  int mkfifoOut = mkfifo(WKP, 644);
+  printf("mkfifoOut = %d\n", mkfifoOut);
+
+  // opening WKP
+  int WKPid = open(WKP, O_RDONLY);
+
+  // reads PP from WKP
+  char* PP = (char*) malloc(20 * sizeof(char));
+  read(WKPid, PP, 20);
+  printf("server received: %s\n", PP);
+
+  // remove WKP
+  remove(WKP);
+
+  // open and return PP
+  from_client = open(PP, O_WRONLY);
+
   return from_client;
 }
 
 /*=========================
-  server_handshake 
+  server_handshake
   args: int * to_client
 
   Performs the server side pipe 3 way handshake.
@@ -25,6 +44,11 @@ int server_setup() {
   =========================*/
 int server_handshake(int *to_client) {
   int from_client;
+
+  server_setup();
+
+
+
   return from_client;
 }
 
@@ -40,21 +64,22 @@ int server_handshake(int *to_client) {
   =========================*/
 int client_handshake(int *to_server) {
   int from_server;
+
+  int pid = getpid();
+  char* PP = (char*) malloc(20 * sizeof(char));
+  sprintf(PP, "%d", pid);
+
+  // making PP
+  mkfifo(PP, 0644);
+
+  // opening WKP
+  int WKPid = open(WKP, O_WRONLY);
+
+  // writing PP to WKP
+  write(WKPid, PP, sizeof(WKPid));
+
+  // opening PP
+  int PPid = open(PP, O_RDONLY);
+
   return from_server;
 }
-
-
-/*=========================
-  server_connect
-  args: int from_client
-
-  handles the subserver portion of the 3 way handshake
-
-  returns the file descriptor for the downstream pipe.
-  =========================*/
-int server_connect(int from_client) {
-  int to_client  = 0;
-  return to_client;
-}
-
-
